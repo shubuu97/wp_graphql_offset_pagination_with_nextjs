f9312ef247e9530b55f83c726b8bb355825ea332
Shubham Chitransh <shubhamchitransh8@gmail.com>
2020-09-15 15:49:26 +0530

    Add Load More

--- gql/getPosts.js
+++ gql/getPosts.js
@@ -1,28 +1,18 @@
 import { gql } from "@apollo/client";
 
 export const GET_POSTS = gql`
-   query getPosts($perPage: Int, $offset: Int) {
-      posts(where: { offsetPagination: { size: $perPage, offset: $offset } }) {
-         pageInfo {
-            offsetPagination {
-               total
-            }
-         }
-         nodes {
-            title
-         }
+  query getPosts($first: Int, $after: String) {
+    posts(first: $first, after: $after) {
+      nodes {
+        id
+        title
       }
-   }
-`;
-
-export const GET_TOTAL_COUNT = gql`
-   query getTotalCount {
-      posts {
-         pageInfo {
-            offsetPagination {
-               total
-            }
-         }
+      edges {
+        cursor
       }
-   }
+      pageInfo {
+        hasNextPage
+      }
+    }
+  }
 `;
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,82 @@
 import React from "react";
-import Link from "next/link";
+import { useQuery } from "@apollo/client";
+import { GET_POSTS } from "../gql/getPosts";
+import client from "../apollo/client";
 
-const Home = () => {
-   return (
-      <div
-         style={{
-            display: "flex",
-            justifyContent: "center",
-            marginTop: "50px",
-         }}
-      >
-         <Link href="/posts/[page_no]" as="/posts/1">
-            <a style={{ fontSize: "32px", color: "blue", textDecoration: "none" }}>Blog Posts</a>
-         </Link>
-      </div>
-   );
+const PER_PAGE = 10;
+
+//? GET_POSTS query variables
+const variables = {
+  first: PER_PAGE,
+  after: null,
+};
+
+//? Gets called on load more, merges the old results with the new one
+const updateQuery = (previousResult, { fetchMoreResult }) => {
+  return {
+    ...fetchMoreResult,
+    posts: {
+      ...(fetchMoreResult?.posts ?? {}),
+      nodes: [
+        ...(previousResult?.posts?.nodes ?? []),
+        ...(fetchMoreResult?.posts?.nodes ?? []),
+      ],
+    },
+  };
+};
+
+const Home = ({ dataServer }) => {
+  const { data: dataClient, error, loading, fetchMore } = useQuery(GET_POSTS, {
+    variables,
+    ssr: false,
+  });
+
+  //? Using getServerSideProps data (dataServer) for the initial Load otherwise using data from useQuery(dataClient)
+  let postsData = typeof window === "undefined" ? dataServer : dataClient;
+
+  const { edges, nodes, pageInfo } = postsData?.posts ?? {};
+  const { hasNextPage } = pageInfo || {};
+
+  return (
+    <div style={{ marginLeft: "50px" }}>
+      <ul>
+        {(nodes || []).map((post, index) => {
+          const { id, title } = post || {};
+          return (
+            <li style={{ padding: "20px" }} key={id}>
+              {title}
+            </li>
+          );
+        })}
+      </ul>
+
+      {hasNextPage && (
+        <button
+          onClick={() => {
+            fetchMore({
+              variables: {
+                ...variables,
+                after: edges?.[edges.length - 1]?.cursor ?? "",
+              },
+              updateQuery,
+            });
+          }}
+        >
+          Load More
+        </button>
+      )}
+    </div>
+  );
 };
 
 export default Home;
+
+export async function getServerSideProps() {
+  const { data } = await client.query({
+    query: GET_POSTS,
+    variables,
+  });
+  return {
+    props: { dataServer: data },
+  };
+}
